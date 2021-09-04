493ffbe8a4922466275be07813410fb264282c82
sega4revenge <sega4revenge@gmail.com>
2021-09-04 13:36:54 +0700

    Initial Commit

---
+++ QuickTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuickTest
+//
+//  Created by Tô Tử Siêu on 04/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
